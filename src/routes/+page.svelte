84244c4177e8ecf1c3f797d2fe427780a4aaf706
Selaries Raphael <raphael.selaries@ensaama.net>
2025-01-28 14:33:36 +0100

    Added background image and video to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import background from "$lib/assets/background.png"
+    import IntroDreamscapeVideo from "./IntroDreamscapeVideo.svelte";
+</script>
+
+
+<div class="background filter_nearest"></div>
+
+<IntroDreamscapeVideo />
+
+
+<style>
+    @import "../lib/global.css";
+
+    .background {
+        z-index: -1;
+        position: fixed;
+        top: 0;
+        height: 100vh;
+        width: 100vw;
+        background-image: url("../lib/assets/background.png");
+        background-position: center;
+        background-size: cover;
+    }
+</style>
