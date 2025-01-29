dbfd2dd2d9820af675976e1887cc08f1403ee398
Selaries Raphael <raphael.selaries@ensaama.net>
2025-01-29 22:26:57 +0100

    ...

--- src/routes/main_page_elements/ProjectCarousel.svelte
+++ src/routes/main_page_elements/ProjectCarousel.svelte
@@ -28,7 +28,7 @@
     </div>
 </div>
 <div class="projects-button-wrapper">
-    <!-- <a href={`${base}/projets`}>Tous les projets</a> -->
+    <a href="https://www.youtube.com">Tous les projets</a>
 </div>
 
 
--- src/routes/main_page_elements/SmallProject.svelte
+++ src/routes/main_page_elements/SmallProject.svelte
@@ -1,12 +1,14 @@
 <script>
     let { name, subtitle, image, imageAlt, style, sidePadding, rightPadding } = $props()
+
+    import imageDebug from "$lib/assets/background.png"
 </script>
 
 <div class="wrapper" style={style + `--sides-padding: ${sidePadding}px; --projects-right-padding: ${rightPadding}px;`}>
     <div class="small_project">
         <h1>{name}</h1>
         <h2>{subtitle}</h2>
-        <!-- <img src={image} alt={imageAlt}> -->
+        <img src={imageDebug} alt={imageAlt}>
     </div>
 </div>
 
