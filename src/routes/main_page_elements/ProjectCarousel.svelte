cfbac61a3f2a9454c4bc6432c4465fedaeffae1e
Selaries Raphael <raphael.selaries@ensaama.net>
2025-01-29 22:23:53 +0100

    Okay, removed the button actually

--- src/routes/main_page_elements/ProjectCarousel.svelte
+++ src/routes/main_page_elements/ProjectCarousel.svelte
@@ -28,7 +28,7 @@
     </div>
 </div>
 <div class="projects-button-wrapper">
-    <a href={`${base}/projets`}>Tous les projets</a>
+    <!-- <a href={`${base}/projets`}>Tous les projets</a> -->
 </div>
 
 
