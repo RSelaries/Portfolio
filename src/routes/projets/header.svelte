28597d7a99c6d341881375e065ee937cfec50709
Selaries Raphael <raphael.selaries@ensaama.net>
2025-02-01 21:39:28 +0100

    Created a header

---
+++ src/routes/projets/header.svelte
@@ -0,0 +1,55 @@
+<!-- @component
+    This component needs the current page to include the global.css
+-->
+<script>
+    import { base } from "$app/paths"
+
+    let { opacity = 1 } = $props()
+</script>
+
+
+<header style="opacity: {opacity};">
+    <div class="buttons">
+        <a href="{base}/">Home</a>
+        <a href="{base}/projets">Projets</a>
+        <div style="width: 100%;" id="button-spacer"></div>
+        <a href="{base}/#a-propos">À propos</a>
+    </div>
+    <div class="line"></div>
+</header>
+
+
+<style>
+    header {
+        font-family: Movement;
+        color: white;
+        top: 0;
+        position: fixed;
+        width: 100vw;
+    }
+
+    .buttons {
+        display: flex;
+        align-items: center;
+        padding: 0 30px;
+    }
+
+    .line {
+        width: 100%;
+        height: 1px;
+        background-color: #555;
+    }
+
+    a {
+        padding: 10px;
+        font-size: 1.3rem;
+        text-decoration: none;
+        color: #0000;
+        font-weight: 900;
+
+        -webkit-text-stroke: white 0.6px;
+        
+        white-space: nowrap;
+    }
+
+</style>
