91da97b2c7da79540bc43a3b6cce91ed28ab8955
Selaries Raphael <raphael.selaries@ensaama.net>
2025-02-04 15:54:15 +0100

    Update dreamscape page

--- src/routes/projets/dreamscape/+page.svelte
+++ src/routes/projets/dreamscape/+page.svelte
@@ -1,11 +1,20 @@
+<script>
+    import { base } from "$app/paths"
+</script>
+
+
 <section class="title-section">
     <h1>Dreamscape</h1>
+    <img src="{base}/projects/assets/dreamscape.gif" alt="">
 </section>
 
 
 <style>
+    @import "../../../lib/global.css";
+
     .title-section {
         width: 100vw;
+        min-height: 100vh;
         color: white;
         font-family: Lexend;
 
